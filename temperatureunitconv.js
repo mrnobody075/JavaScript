f37cb3cf3146525d3b52e58a2223377dceec3b73
mrnobody075 <hegdevishals75@gmail.com>
2024-08-02 23:22:30 +0530

    Done

---
+++ temperatureunitconv.js
@@ -0,0 +1,9 @@
+var kelvin = 293;
+//thatsall
+var celcius = 273-kelvin;
+//thatsall
+var fahrenheit = celcius*(9/5) + 32;
+//thasall
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+
