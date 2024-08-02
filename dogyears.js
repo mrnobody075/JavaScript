790bd4d91535a3ce9b69856d1199791a54f88026
mrnobody075 <hegdevishals75@gmail.com>
2024-08-02 23:36:14 +0530

    practice

---
+++ dogyears.js
@@ -0,0 +1,11 @@
+const myAge = 20;
+let earlyYears = 2;
+earlyYears = earlyYears * 10.5; // Multiply earlyYears by 10.5
+
+let laterYears = myAge - 2;
+laterYears = laterYears * 4; // Multiply laterYears by 4
+
+const myAgeInDogYears = earlyYears + laterYears;
+const myName = 'Mrnody'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
